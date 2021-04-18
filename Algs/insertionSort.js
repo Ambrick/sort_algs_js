f71f017227dbed0846bc8c3abf6f6cc3a64a3f65
Ambrick <mrfaster98@gmail.com>
2021-04-18 13:34:18 +0300

    English comments added

--- Algs/insertionSort.js
+++ Algs/insertionSort.js
@@ -1,13 +1,14 @@
-/*Сортировка вставками
+/*Insertion sort
 -Worst-case performance:	О(n2) comparisons and swaps
 -Best-case performance:	  O(n)  comparisons, O(1) swaps
 -Average performance:   	О(n2) comparisons and swaps
 */
 export function sort(arr) {
-  //Во внешнем цикле i - индекс границы отсортированного массива
   for (let i = 1; i < arr.length; i++) {
+    //j - boundary index between sorted and unsorted parts
     let j = i;
     //Пока эл. из несортированной части < эл. из сортированной части
+    //Swap el's till el from unsorted part > than el from sorted part
     while (arr[j] < arr[j - 1]) {
       [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]];
       j--;
