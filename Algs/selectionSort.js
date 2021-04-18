3c51ad0487cf070e56cd054e2928a601c1cfa251
Ambrick <mrfaster98@gmail.com>
2021-04-18 13:30:32 +0300

    English comments added

--- Algs/selectionSort.js
+++ Algs/selectionSort.js
@@ -1,18 +1,15 @@
-/*Сортировка выбором
+/*Selection sort
 -Worst-case performance:	О(n2) comparisons, О(n) swaps
 -Best-case performance:   О(n2) comparisons, O(1) swaps
 -Average performance:	    О(n2) comparisons, О(n) swaps
 */
 export function sort (arr) {
-  //Во внешнем цикле i - индекс границы отсортированного массива
-  for (let i = 0; i < arr.length - 1; i++) 
-  { 
-    //Сравниваем каждый последующий элемент с эл. по индексу i + 1
-      for (let j = i + 1; j < arr.length; j++) {
-        if (arr[i] > arr[j]) {
-          [arr[i], arr[j]] = [arr[j], arr[i]];
-        }
-      }
-  }
+  //i - boundary index between sorted and unsorted parts
+  for (let i = 0; i < arr.length - 1; i++)
+    //We choose the lowest element of the ansorted array and put in a boundary position
+    for (let j = i + 1; j < arr.length; j++)
+      if (arr[i] > arr[j]) 
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+
   return arr;
 };
