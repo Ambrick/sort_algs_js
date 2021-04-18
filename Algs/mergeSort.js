e9888214b6b8253cc6a506557c4c9d2d085afba5
Ambrick <mrfaster98@gmail.com>
2021-04-18 13:16:38 +0300

    Fixed cutting into two parts, english comments added

--- Algs/mergeSort.js
+++ Algs/mergeSort.js
@@ -1,46 +1,39 @@
-/*Сортировка слиянием
+/*Merge sort
 -Worst-case performance:  O(n(log n))
 -Best-case performance:   О(n2)
 -Average performance:	    О(n2)
 */
 export function sort(arr) {
-  //Проверка на размер полученного массива
+  //If array is empty or has only 1 el, than return that array
   if (arr.length < 2) return arr;
-  //Находим середину массива
-  const middle = Math.floor(arr.length / 2);
-  //Объединяем отсортированные куски(половинки от оригинальной строки) для возврата
-  return mergeArrays(sort(arr.slice(0, middle)), arr.slice(middle));
+  //Cut the half of the array and sort it, than sort the remaining part,
+  //than merge theese sorted parts
+  return mergeArrays(sort(arr.splice(0, Math.floor(arr.length / 2))), sort(arr));
 };
 
 function mergeArrays(l_arr, r_arr){
   const mergedArr = [];
-  //Сравниваем первые элементы массивов, наименьший
-  //заносим в конечный массив
+  //Check the lowest el in first indexes of left and right arrays,
+  //than push it to the new "mergedArray"
   while (l_arr.length && r_arr.length) {
     mergedArr.push(l_arr[0] > r_arr[0] ? r_arr.shift() : l_arr.shift());
   }
-  //Возвращаем отсортированный массив, присоедияния возможные остатки 
-  //от левого и правого массивов
+  //Merge to sorted array remainings from left and right parts, there can be leftovers after previous step
   return mergedArr.concat(l_arr, r_arr);
-  /*
-  //Сравниваем первые элементы левого и правого массивов
-  //и отщипляем меньший из них в конечный массив
+
+  /*--Alternative way--
   while (l_arr.length && r_arr.length) {
     if (l_arr[0] > r_arr[0]) {
       mergedArr.push(r_arr.shift());
     } else {
       mergedArr.push(l_arr.shift());
     }
-    //mergedArr.push(l_arr[0] > r_arr[0] ? r_arr.shift() : l_arr.shift());
   }
-  //После сравнений, в одном из массивов могут оставаться
-  //элементы
   while (l_arr.length) {
     mergedArr.push(l_arr.shift());
   }
   while (r_arr.length) {
     mergedArr.push(r_arr.shift());
   }
-  return mergedArr;
-  */
+  return mergedArr;*/
 };
