37094baea2651b89360df9f31ef50328c1219ed1
Ambrick <mrfaster98@gmail.com>
2021-05-09 12:02:25 +0300

    Wrong performance values were fixed, another realization was removed

--- Algs/mergeSort.js
+++ Algs/mergeSort.js
@@ -1,14 +1,14 @@
 /*Merge sort
 -Worst-case performance:  O(n(log n))
--Best-case performance:   О(n2)
--Average performance:	    О(n2)
+-Best-case performance:   О(n(log n))
+-Average performance:	    О(n(log n))
 */
 export function sort(arr) {
   //If array is empty or has only 1 el, than return that array
   if (arr.length < 2) return arr;
   //Cut the half of the array and sort it, than sort the remaining part,
   //than merge theese sorted parts
-  return mergeArrays(sort(arr.splice(0, Math.floor(arr.length / 2))), sort(arr));
+  return mergeArrays(sort(arr.splice(0, Math.floor(arr.length / 2))), sort(arr)) ;
 };
 
 function mergeArrays(l_arr, r_arr){
@@ -20,20 +20,4 @@ function mergeArrays(l_arr, r_arr){
   }
   //Merge to sorted array remainings from left and right parts, there can be leftovers after previous step
   return mergedArr.concat(l_arr, r_arr);
-
-  /*--Alternative way--
-  while (l_arr.length && r_arr.length) {
-    if (l_arr[0] > r_arr[0]) {
-      mergedArr.push(r_arr.shift());
-    } else {
-      mergedArr.push(l_arr.shift());
-    }
-  }
-  while (l_arr.length) {
-    mergedArr.push(l_arr.shift());
-  }
-  while (r_arr.length) {
-    mergedArr.push(r_arr.shift());
-  }
-  return mergedArr;*/
 };
