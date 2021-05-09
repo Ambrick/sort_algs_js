10dffad803e5d28f9c76eb0d7e377eaff7ae3d5f
Ambrick <mrfaster98@gmail.com>
2021-05-09 12:19:43 +0300

    New description was added

--- Algs/mergeSort.js
+++ Algs/mergeSort.js
@@ -1,8 +1,15 @@
-/*Merge sort
--Worst-case performance:  O(n(log n))
--Best-case performance:   О(n(log n))
--Average performance:	    О(n(log n))
-*/
+/**
+ * The implementation of `Merge sort`.
+ *
+ *-Worst-case performance:  O(n(log n))
+ *-Best-case performance:   О(n(log n))
+ *-Average performance:	    О(n(log n))
+ *
+ * @private
+ * @param {arr} object The Array to sort.
+ * @returns {Array} Returns `Array`.
+ */
+
 export function sort(arr) {
   //If array is empty or has only 1 el, than return that array
   if (arr.length < 2) return arr;
