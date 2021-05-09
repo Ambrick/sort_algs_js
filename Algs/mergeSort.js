c8175119e6a53ae0e0c15288d3e3baf2b2d1d7b0
Ambrick <mrfaster98@gmail.com>
2021-05-09 12:08:38 +0300

    Function mergeArrays was placed into sort function

--- Algs/mergeSort.js
+++ Algs/mergeSort.js
@@ -6,18 +6,19 @@
 export function sort(arr) {
   //If array is empty or has only 1 el, than return that array
   if (arr.length < 2) return arr;
+  
+  function mergeArrays(l_arr, r_arr) {
+    const mergedArr = [];
+    //Check the lowest el in first indexes of left and right arrays,
+    //than push it to the new "mergedArray"
+    while (l_arr.length && r_arr.length) {
+      mergedArr.push(l_arr[0] > r_arr[0] ? r_arr.shift() : l_arr.shift());
+    }
+    //Merge to sorted array remainings from left and right parts, there can be leftovers after previous step
+    return mergedArr.concat(l_arr, r_arr);
+  };
   //Cut the half of the array and sort it, than sort the remaining part,
   //than merge theese sorted parts
-  return mergeArrays(sort(arr.splice(0, Math.floor(arr.length / 2))), sort(arr)) ;
+  return mergeArrays(sort(arr.splice(0, Math.floor(arr.length / 2))), sort(arr));
 };
 
-function mergeArrays(l_arr, r_arr){
-  const mergedArr = [];
-  //Check the lowest el in first indexes of left and right arrays,
-  //than push it to the new "mergedArray"
-  while (l_arr.length && r_arr.length) {
-    mergedArr.push(l_arr[0] > r_arr[0] ? r_arr.shift() : l_arr.shift());
-  }
-  //Merge to sorted array remainings from left and right parts, there can be leftovers after previous step
-  return mergedArr.concat(l_arr, r_arr);
-};
