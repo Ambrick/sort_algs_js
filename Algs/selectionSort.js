c80f84bb5a7f6fda491413585e09c70e8a4f2c51
Ambrick <mrfaster98@gmail.com>
2021-05-14 01:24:07 +0300

    Comments added

--- Algs/selectionSort.js
+++ Algs/selectionSort.js
@@ -1,15 +1,24 @@
-/*Selection sort
--Worst-case performance:	О(n2) comparisons, О(n) swaps
--Best-case performance:   О(n2) comparisons, O(1) swaps
--Average performance:	    О(n2) comparisons, О(n) swaps
-*/
-export function sort (arr) {
+/**
+ * The implementation of `Selection sort`.
+ *
+ *-Best-case performance:   О(n2) comparisons, O(1) swaps
+ *-Average performance:	    О(n2) comparisons, О(n) swaps
+ *-Worst-case performance:	О(n2) comparisons, О(n) swaps
+ *
+ * @private
+ * @param {arr} object The Array to sort.
+ * @returns {Array} Returns `Array`.
+ */
+
+function selectionSort(arr) {
   //i - boundary index between sorted and unsorted parts
   for (let i = 0; i < arr.length - 1; i++)
-    //We choose the lowest element of the ansorted array and put in a boundary position
     for (let j = i + 1; j < arr.length; j++)
-      if (arr[i] > arr[j]) 
-        [arr[i], arr[j]] = [arr[j], arr[i]];
+    //Choose the lowest element of the ansorted array and put in a boundary position
+        if (arr[i] > arr[j]) 
+          [arr[i], arr[j]] = [arr[j], arr[i]];
 
   return arr;
 };
+
+export default selectionSort
