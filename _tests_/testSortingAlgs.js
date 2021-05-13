5f499a7879acd78e8c16427cc1a7ebb7398839e3
Ambrick <mrfaster98@gmail.com>
2021-05-14 01:25:14 +0300

    Updated

--- _tests_/testSortingAlgs.js
+++ _tests_/testSortingAlgs.js
@@ -1,23 +1,25 @@
-import { expect, } from 'chai';
-import _ from 'lodash';
+import {expect} from 'chai';
 
-import { sort as bubbleSort, opt_bubblesort as optBubbleSort} from '../Algs/bubbleSort.js';
-import { sort as quickSort, opt_qsort as optQuickSort} from '../Algs/quickSort.js';
-import { sort as insertionSort} from '../Algs/insertionSort.js';
-import { sort as mergeSort} from '../Algs/mergeSort.js';
-import { sort as selectionSort} from '../Algs/selectionSort.js';
+import bubbleSort from '../Algs/bubbleSort.js';
+import optimizedBubbleSort from '../Algs/optimizedBubbleSort.js';
+import quickSort from '../Algs/quickSort.js';
+import optimizedQuickSort from '../Algs/optimizedQuickSort.js';
+import insertionSort from '../Algs/insertionSort.js';
+import mergeSort from '../Algs/mergeSort.js';
+import selectionSort from '../Algs/selectionSort.js';
 
 export function testSortingAlgs() {
-  const arr = [11,23,100,6,2,7,9,10,55,1,8,4,5,3,17,56];
-  const arrSorted = _.sortBy(arr);
+  const arr = [11, 23, 100, 6, 2, 7, 9, 10, 55, 1, 8, 4, 5, 3, 17, 56];
+  const arrSorted = arr;
+  arrSorted.sort((a, b ) => a - b);
 
-  expect(bubbleSort(arr.slice()))   .to.eql(arrSorted, 'Array, sorted by bubbleSort != sortArray');
-  expect(optBubbleSort(arr.slice())).to.eql(arrSorted, 'Array, sorted by optBubbleSort != sortArray');
-  expect(quickSort(arr.slice()))    .to.eql(arrSorted, 'Array, sorted by quickSort != sortArray');
-  expect(optQuickSort(arr.slice())) .to.eql(arrSorted, 'Array, sorted by optQuickSort!= sortArray');
-  expect(insertionSort(arr.slice())).to.eql(arrSorted, 'Array, sorted by insertionSort != sortArray');
-  expect(selectionSort(arr.slice())).to.eql(arrSorted, 'Array, sorted by selectionSort != sortArray');
-  expect(mergeSort(arr.slice()))    .to.eql(arrSorted, 'Array, sorted by mergeSort != sortArray');
+  expect(bubbleSort(arr.slice()))         .to.eql(arrSorted, 'Array, sorted by bubbleSort != sortArray');
+  expect(optimizedBubbleSort(arr.slice())).to.eql(arrSorted, 'Array, sorted by optBubbleSort != sortArray');
+  expect(quickSort(arr.slice()))          .to.eql(arrSorted, 'Array, sorted by quickSort != sortArray');
+  expect(optimizedQuickSort(arr.slice())) .to.eql(arrSorted, 'Array, sorted by optQuickSort!= sortArray');
+  expect(insertionSort(arr.slice()))      .to.eql(arrSorted, 'Array, sorted by insertionSort != sortArray');
+  expect(selectionSort(arr.slice()))      .to.eql(arrSorted, 'Array, sorted by selectionSort != sortArray');
+  expect(mergeSort(arr.slice()))          .to.eql(arrSorted, 'Array, sorted by mergeSort != sortArray');
 };
 
 testSortingAlgs();
